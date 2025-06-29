e81d1a097b152bcb0d80061db731841f3c746a39
Daffa <Daffafariq@gmail.com>
2025-06-29 18:41:48 +0800

    implement vapi header

--- lib/actions/companion.action.ts
+++ lib/actions/companion.action.ts
@@ -54,3 +54,16 @@ export const getAllCompanions = async ({
     return [];
   }
 };
+
+export const getCompanion = async (id: string) => {
+  const supabase = createSupabaseClient()
+
+  const { data, error } = await supabase 
+    .from('companions')
+    .select()
+    .eq('id', id)
+  
+  if (error) return console.log(error)
+  
+  return data[0]
+}
