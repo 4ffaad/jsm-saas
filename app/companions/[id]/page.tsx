39e78f64790e060f07f979bd5e8e6738bf64c573
Daffa <Daffafariq@gmail.com>
2025-07-01 18:47:24 +0800

    implement vapi

--- app/companions/[id]/page.tsx
+++ app/companions/[id]/page.tsx
@@ -1,37 +1,33 @@
-import { getCompanion } from "@/lib/actions/companion.action"
-import { getSubjectColor } from "@/lib/utils"
-import { currentUser } from "@clerk/nextjs/server"
-import { redirect } from "next/navigation"
+import {getCompanion} from "@/lib/actions/companion.action";
+import {currentUser} from "@clerk/nextjs/server";
+import {redirect} from "next/navigation";
+import {getSubjectColor} from "@/lib/utils";
 import Image from "next/image";
+import CompanionComponent from "@/components/CompanionComponent";
 
-
-interface CompnaionSessionPageProps {
-  params: Promise<{id:string}>
+interface CompanionSessionPageProps {
+    params: Promise<{ id: string}>;
 }
- 
-const CompanionSession = async ({ params }: CompnaionSessionPageProps) => {
-  const { id } = await params
-  const {name,subject,topic,duration,title} = await getCompanion(id)
-  const user = await currentUser()
 
-  if (!user) redirect('/sign-in')
-  if(!name) redirect ('/companion')
+const CompanionSession = async ({ params }: CompanionSessionPageProps) => {
+    const { id } = await params;
+    const companion = await getCompanion(id);
+    const user = await currentUser();
 
-  return (
-    <main>
-      <article className="flex rounded-border justify-between p-6 max-md:flex-col">
-        <div className="flex items-center gap-2">
-          <div
-            className="size-[72px] flex items-center justify-center rounded-lg max-md:hidden"
-            style={{backgroundColor:getSubjectColor(subject)}}>
-            <Image
-              src={`/icons/${subject}.svg`}
-              alt={subject}
-              width={35}
-              height = {35}
-            /> 
-          </div>
-           <div className="flex flex-col gap-2">
+    const { name, subject, title, topic, duration } = companion;
+
+    if(!user) redirect('/sign-in');
+    if(!name) redirect('/companions')
+
+    return (
+        <main>
+            <article className="flex rounded-border justify-between p-6 max-md:flex-col">
+                <div className="flex items-center gap-2">
+                    <div className="size-[72px] flex items-center justify-center rounded-lg max-md:hidden" style={{ backgroundColor: getSubjectColor(subject)}}>
+                        <Image src={`/icons/${subject}.svg`} alt={subject} width={35} height={35} />
+                    </div>
+
+                    <div className="flex flex-col gap-2">
                         <div className="flex items-center gap-2">
                             <p className="font-bold text-2xl">
                                 {name}
@@ -47,8 +43,15 @@ const CompanionSession = async ({ params }: CompnaionSessionPageProps) => {
                     {duration} minutes
                 </div>
             </article>
-    </main>
-  )
+
+            <CompanionComponent
+                {...companion}
+                companionId={id}
+                userName={user.firstName!}
+                userImage={user.imageUrl!}
+            />
+        </main>
+    )
 }
 
 export default CompanionSession
